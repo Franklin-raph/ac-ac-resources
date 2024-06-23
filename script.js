f452c668333fd68d423b68395973f69c8c086da5
OneTrueHomie <my.onetruehomie@gmail.com>
2024-06-23 16:39:59 +0100

    mobile responsive and menu fixed

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// $(document).ready(function(){
-// });
 $(".names").owlCarousel({
     items: 3,
     loop: true,
@@ -27,3 +25,42 @@ $(".names").owlCarousel({
         }
     }
 });
+
+const navOpen = document.querySelector(".bi-list");
+const navClose = document.querySelector(".bi-x-lg");
+const sideBar = document.querySelector("nav .sect-2");
+const myMenu = document.querySelectorAll("#sect-2");
+const htmlElement = document.documentElement;
+const bodyElement = document.body;
+
+navOpen.addEventListener("click", () => {
+    navOpen.style.display = "none";
+    navClose.style.display = "block";
+    sideBar.style.display = "block";
+    htmlElement.style.overflow = "hidden";
+
+});
+
+navClose.addEventListener("click", () => {
+    navOpen.style.display = "block";
+    navClose.style.display = "none";
+    sideBar.style.display = "none";
+    htmlElement.style.overflow = "auto";
+});
+
+
+
+const anchorTags = document.querySelectorAll("nav .sect-2 ul li");
+anchorTags.forEach(anchor => {
+    anchor.addEventListener("click", () => {
+        sideBar.style.display = "none";
+        htmlElement.style.overflow = "auto";
+        navClose.style.display = "none";
+        navOpen.style.display = "block";
+    });
+});
+
+
+
+
+
